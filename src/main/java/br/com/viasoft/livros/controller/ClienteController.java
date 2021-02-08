a7516750e4a240dee59dd3ab78c517244402488c
Filipe <filipewk@gmail.com>
2021-02-08 16:46:06 -0200

    create Cliente Controller

---
+++ src/main/java/br/com/viasoft/livros/controller/ClienteController.java
@@ -0,0 +1,27 @@
+package br.com.viasoft.livros.controller;
+
+import br.com.viasoft.livros.model.Cliente;
+import br.com.viasoft.livros.model.Produto;
+import br.com.viasoft.livros.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping("/cliente")
+    public String getHome(Model model) {
+
+        List<Cliente> lista = clienteRepository.findAll();
+        model.addAttribute("lista", lista);
+
+        return "cliente";
+    }
+}
